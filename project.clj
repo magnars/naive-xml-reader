a19bd0b2e38ee46af04ae0ea7f7fcb76edfb6c5f
Magnar Sveen <magnars@gmail.com>
2016-02-22 20:04:17 +0100

    Remove unused dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/data.zip "0.1.1"]
                  [camel-snake-kebab "0.3.2"]]
   :profiles {:dev {:dependencies [[expectations "2.1.4"]
                                   [flare "0.2.9"]]
